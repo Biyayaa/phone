da87b1efb690ca99a88db2eff1fbbfa7a75db4f0
Biyayaa <onaleye.ap@gmail.com>
2023-04-11 03:04:33 +0100

    Added current time to homescreen

--- script.js
+++ script.js
@@ -5,6 +5,40 @@ let homepage = document.getElementById('homepage');
 let fullHomepage = document.getElementById('fullHomepage');
 let showAlarm = document.getElementById('showAlarm');
 let try2 = document.getElementById('try');
+let homeTime = document.getElementById('homeTime');
+
+
+
+
+
+
+// phone time
+function updateTime() {
+  date = new Date();
+  seconds = date.getSeconds();
+  hours = date.getHours();
+  minutes = date.getMinutes();
+
+  amPm = hours >= 12 ? "PM" : "AM"; // check if it's AM or PM
+  hours = hours % 12 || 12; // convert to 12-hour format
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  let currentTime = hours + ":" + minutes + ":" + seconds + " " + amPm;
+  time.innerHTML = currentTime;
+
+
+  homeTime.innerHTML = hours + ":" + minutes;
+}
+setInterval(updateTime, 1000);
+
 
 
 
@@ -44,6 +78,7 @@ cameraIcon.addEventListener('click', (async()=>{
   try2.style.display = "none";
   showAlarm.style.display = "none";
   cameraApp.style.display = "block";
+  video.style.display ="block"
   let snap = await navigator.mediaDevices.getUserMedia({audio:false, video:true})
   video.srcObject = snap;
 }))
@@ -60,56 +95,18 @@ shutterButton.addEventListener("click", (()=>{
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let time = document.getElementById("time");
 let date, hours, minutes, seconds, amPm;
 
-function updateTime() {
-  date = new Date();
-  seconds = date.getSeconds();
-  hours = date.getHours();
-  minutes = date.getMinutes();
 
-  amPm = hours >= 12 ? "PM" : "AM"; // check if it's AM or PM
-  hours = hours % 12 || 12; // convert to 12-hour format
 
-  if (hours < 10) {
-    hours = "0" + hours;
-  }
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-  let currentTime = hours + ":" + minutes + ":" + seconds + " " + amPm;
-  time.innerHTML = currentTime;
-}
-setInterval(updateTime, 1000);
+
+
+
+
+
 
 // alarm
-
-
-
-
 let hourss = document.getElementById("hourss");
 let minutess = document.getElementById("minutess");
 let secondss = document.getElementById("secondss");
